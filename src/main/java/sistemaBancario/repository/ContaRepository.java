e3649dcad5b52b729a14f6107579f4da3f9e9d43
Raphael Borges <rah-borges@hotmail.com>
2021-01-24 10:23:22 -0300

    refact(repositoryGeneric):criado CRUD generico #12

--- src/main/java/sistemaBancario/repository/ContaRepository.java
+++ src/main/java/sistemaBancario/repository/ContaRepository.java
@@ -1,48 +1,10 @@
 package sistemaBancario.repository;
 
-import javax.persistence.EntityManager;
-
+import sistemaBancario.generics.RepositoryGeneric;
 import sistemaBancario.models.Conta;
-import sistemaBancario.utils.ConexaoFactory;
 
-public class ContaRepository {
-	private EntityManager em;
-
-	public ContaRepository() {
-		this.em = ConexaoFactory.getConexao();
-	}
-
+public class ContaRepository extends RepositoryGeneric<Conta> {
 	public Conta findById(Integer id) {
 		return em.find(Conta.class, id);
-	}
-
-	public void create(Conta conta) {
-		try {
-			em.getTransaction().begin();
-			em.persist(conta);
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void update(Conta conta) {
-		try {
-			em.getTransaction().begin();
-			em.merge(conta);
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void delete(Conta conta) {
-		try {
-			em.getTransaction().begin();
-			em.remove(conta);
-			em.getTransaction().commit();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 }
