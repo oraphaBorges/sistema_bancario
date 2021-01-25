2f2210d335cc3a54751a18ee91c314b108a87cb3
RafaelMarangoni <rafael.dmarangoni89@gmail.com>
2021-01-25 11:07:13 -0300

    feat (criação dos metódos consulta, pagamento e deposito)

--- src/main/java/sistemaBancario/repository/ContaRepository.java
+++ src/main/java/sistemaBancario/repository/ContaRepository.java
@@ -1,10 +1,18 @@
 package sistemaBancario.repository;
 
+import java.sql.PreparedStatement;
+
+import sistemaBancario.Services.ContaService;
 import sistemaBancario.generics.RepositoryGeneric;
 import sistemaBancario.models.Conta;
 
 public class ContaRepository extends RepositoryGeneric<Conta> {
+		
 	public Conta findById(Integer id) {
 		return em.find(Conta.class, id);
 	}
+	
+	
 }
+
+
