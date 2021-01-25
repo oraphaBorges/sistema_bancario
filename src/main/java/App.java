caef14d2f649a6ff2e14cffe798fbbc452874780
Raphael Borges <oborgesrapha@gmail.com>
2021-01-25 12:15:29 -0300

    refact(contaservice):ajustado contaservice e feito test #21

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 
+import sistemaBancario.Services.ContaService;
 import sistemaBancario.models.Conta;
 import sistemaBancario.models.Sigla;
 import sistemaBancario.models.Usuario;
@@ -12,10 +13,6 @@ import sistemaBancario.utils.ConexaoFactory;
 
 public class App {
 	public static void main(String[] args) {
-		// DATABASE CONNECTION CHECK
-		//EntityManager em = ConexaoFactory.getConexao();
-		
-		// CONTA CRUD
 		Conta conta = new Conta();
 		conta.setNome("ForgenKey");
 		conta.setSaldo(1253.66);
@@ -36,57 +33,32 @@ public class App {
 		user.setSenha("1234");
 		user.addConta(conta);
 		
-		//ASSOAÇÃO DO USUARIO A CONTA 
-		conta.setUsuario(user);
-		
-		UsuarioRepository urep = new UsuarioRepository();
-		//CREATE
-		urep.create(user);
-		System.out.println("CREATED");
-		
-		//FIND
+//		//ASSOAÇÃO DO USUARIO A CONTA 
+//		conta.setUsuario(user);
+//		
+//		UsuarioRepository urep = new UsuarioRepository();
+//		//CREATE
+//		urep.create(user);
+//		System.out.println("CREATED");
+//		
+//		//FIND
 //		Usuario userCheck = urep.findById(user.getId());
 //		System.out.println("FOUND");
 //		System.out.println(userCheck.getNome());
-		
-		//UPDATE
+//		
+//		//UPDATE
 //		user.setNome("novo nome");
 //		urep.update(user);
 //		userCheck = urep.findById(user.getId());
 //		System.out.println("UPDATED");
 //		System.out.println(userCheck.getNome());
-
+//
+	
 		//DELETE
 //		urep.delete(user);
 //		System.out.println("DELETED");
 		
 		
-		// CONTA CRUD
-//		Conta conta = new Conta();
-//		conta.setNome("teste");
-//		conta.setSaldo(1253.66);
-//		conta.setSigla(Sigla.CORRENTE);
-		
-//		ContaRepository crep = new ContaRepository();
-//		//CREATE
-//		crep.create(conta);
-//		System.out.println("CREATED");
-		
-		//FIND
-//		Conta contaCheck = crep.findById(conta.getId());
-//		System.out.println("FOUND");
-//		System.out.println(contaCheck.getSaldo());
-		
-		//UPDATE
-//		conta.setSaldo(2687.99);
-//		crep.update(conta);
-//		contaCheck = crep.findById(conta.getId());
-//		System.out.println("UPDATED");
-//		System.out.println(contaCheck.getSaldo());
-
-		//DELETE
-//		crep.delete(conta);
-//		System.out.println("DELETED");
 		
 	}
 }
