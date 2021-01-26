7a00395212ea646b88f71e3f494920fee8535522
Raphael Borges <rah-borges@hotmail.com>
2021-01-26 11:32:37 -0300

    refact(planoconta):add busca e check #32

--- src/main/java/sistemaBancario/repository/PlanoContaRepository.java
+++ src/main/java/sistemaBancario/repository/PlanoContaRepository.java
@@ -1,5 +1,7 @@
 package sistemaBancario.repository;
 
+import javax.persistence.Query;
+
 import sistemaBancario.generics.RepositoryGeneric;
 import sistemaBancario.models.PlanoConta;
 
@@ -7,4 +9,15 @@ public class PlanoContaRepository extends RepositoryGeneric<PlanoConta> {
 	public PlanoConta findById(Integer id) {
 		return em.find(PlanoConta.class, id);
 	}
+
+	public PlanoConta findByFinalidade(String finalidade) {
+		return em.find(PlanoConta.class, finalidade);
+	}
+
+	public boolean exists(String finalidade) {
+		Query query = em.createQuery("select u from PlanoConta u where u.finalidade = :finalidade"); // JPQL
+		query.setParameter("finalidade", finalidade);
+		return query.getResultList().size() != 0;
+	}
+
 }
