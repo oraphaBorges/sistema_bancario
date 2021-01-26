cd621bed432d5146185ff3de96c3955840d1b549
Raphael Borges <rah-borges@hotmail.com>
2021-01-26 14:49:14 -0300

    test(extrato):mostrando o extrato #26

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,69 +1,40 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import sistemaBancario.Services.ContaService;
-import sistemaBancario.Services.LancamentoService;
+import sistemaBancario.Services.UsuarioService;
 import sistemaBancario.models.Conta;
-import sistemaBancario.models.Sigla;
+import sistemaBancario.models.Lancamento;
 import sistemaBancario.models.Usuario;
-import sistemaBancario.repository.ContaRepository;
-import sistemaBancario.repository.LancamentoRepository;
-import sistemaBancario.repository.UsuarioRepository;
 
 public class App {
-	private static ContaRepository crep = new ContaRepository();
-	private static UsuarioRepository urep = new UsuarioRepository();
 	
-
+	private static ContaService contaService = new ContaService();
+	private static UsuarioService usuarioService = new UsuarioService();
+	
 	public static void main(String[] args) {
-		// CONTA CREATE
-//		Conta conta = new Conta();
-//		conta.setNome("ForgenKey");
-//		conta.setSaldo(1253.66);
-//		conta.setSigla(Sigla.CORRENTE);
-//		
-//		Conta conta2 = new Conta();
-//		conta.setNome("Conta2");
-//		conta.setSaldo(10.00);
-//		conta.setSigla(Sigla.CORRENTE);
-//
-//		crep.create(conta);
-//		crep.create(conta2);
-
-		// USUARIO CREATE
-//		Usuario user = new Usuario();
-//		user.setLogin("Tranferencia Origem");
-//		user.setCpf("11111111111");
-//		user.setNome("Origem");
-//		user.setSenha("1234");
-//		user.addConta(conta);
-//		urep.create(user);
-//		
-//		Usuario user2 = new Usuario();
-//		user.setLogin("Tranferencia Recebdora");
-//		user.setCpf("11111111111");
-//		user.setNome("recebedor");
-//		user.setSenha("1234");
-//		user.addConta(conta);
-//		urep.create(user2);
-
-		// CONTA ASSOSIATION
-//		conta.setUsuario(user);
-//		crep.update(conta);
-//		
-//		conta.setUsuario(user2);
-//		crep.update(conta2);
+		ArrayList<Lancamento> extrato1 = new ArrayList<Lancamento>();
+		ArrayList<Lancamento> extrato2 = new ArrayList<Lancamento>();
 		
-		//LANCAMENTO TRANSFERENCIA && LANCAMENTO PAGAMENTO
-		LancamentoService lser = new LancamentoService();
-		lser.transferencia("teste transferencia", 2, 1, 500.00, "testando aplicação");
+		LocalDate inicio = LocalDate.of(2000,3,7);
+		LocalDate fim = LocalDate.of(2019,1,7);
 		
-		lser.pagamento(5000.00, 2, "superMercado", "contas mensais");
+		Usuario usuario = usuarioService.buscarUsuario("jisoo.black");
 		
-//		user = urep.findById(1);
-//		ContaService csvc = new ContaService();
-//		System.out.println(user.getContas().get(0).getSaldo());
-//		csvc.depositar(user.getContas().get(0), 150);
-//		System.out.println(user.getContas().get(0).getSaldo());
-//		csvc.pagar(user.getContas().get(0), 500);
-//		System.out.println(user.getContas().get(0).getSaldo());
+		Conta conta = usuario.getContas().get(0);
+		
+		extrato1 = contaService.consultarExtrato(conta);
+		extrato2 = contaService.consultarExtrato(conta,inicio,fim);
+		
+		System.out.println("Exrato Geral");
+		for(Lancamento e: extrato1) {
+			System.out.println(e.getContaOrigem().getId()+ "\t" + e.getId() + "\t" + e.getValor() + "\t" + e.getDataLancamento().toString() );
+		}
+		
+		System.out.println("\nExtrado de " + inicio.toString() + " à "+ fim.toString());
+		for(Lancamento e: extrato2) {
+			System.out.println(e.getContaOrigem().getId()+ "\t" +e.getId() + "\t" + e.getValor() + "\t" + e.getDataLancamento().toString() );
+		}
 
 	}
 }
