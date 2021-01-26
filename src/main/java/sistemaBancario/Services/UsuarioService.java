b92ad3bd6188627b7765e3676dbad31125201b25
Raphael Borges <rah-borges@hotmail.com>
2021-01-26 11:38:12 -0300

    feat(usuarioservices):criado uṕdate e delete #33

--- src/main/java/sistemaBancario/Services/UsuarioService.java
+++ src/main/java/sistemaBancario/Services/UsuarioService.java
@@ -1,6 +1,5 @@
 package sistemaBancario.Services;
 
-
 import sistemaBancario.models.Usuario;
 import sistemaBancario.repository.UsuarioRepository;
 
@@ -8,19 +7,28 @@ public class UsuarioService {
 
 	private UsuarioRepository repository = new UsuarioRepository();
 
-				
-		public void incluir(Usuario usuario) {
-			
-			if(repository.exists(usuario.getLogin())) {
-				throw new IllegalStateException("JÃ¡ existe um usuario com o login " + usuario.getLogin());
-				
-		}else {
-			repository.create(usuario);
-		}	
-			System.out.println("FIM");
-		}
-			
-		
+	public void cadastrar(Usuario usuario) {
+
+		if (repository.exists(usuario.getLogin()))
+			throw new IllegalStateException("JÃ¡ existe um usuario com o login " + usuario.getLogin());
+
+		repository.create(usuario);
+
+	}
+
+	public Usuario buscarUsuario(String login) {
+		return repository.findByLogin(login);
+	}
+	public Usuario buscarUsuario(Integer id) {
+		return repository.findById(id);
+	}
+
+	public void alterarCadastro(Usuario usuario) {
+		repository.update(usuario);		
+	}
+
+	public void excluirCadastro(Usuario usuario) {
+		repository.delete(usuario);		
+	}
 
 }
-
