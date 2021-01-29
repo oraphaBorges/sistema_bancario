2f616446230302d765f861d7300fc599fa33713e
Raphael Borges <rah-borges@hotmail.com>
2021-01-29 15:35:03 -0300

    refact(seed):add command line pra add user #58

--- src/main/java/sistemaBancario/SBootApplication.java
+++ src/main/java/sistemaBancario/SBootApplication.java
@@ -8,5 +8,17 @@ public class SBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SBootApplication.class, args);
 	}
+	
+//	@Bean
+//	public CommandLineRunner run(UsuarioService s) throws Exception {
+//		return args -> {
+//			Usuario u = new Usuario();
+//			u.setCpf("11111111111");
+//			u.setNome("admin");
+//			u.setLogin("admin");
+//			u.setSenha("admin");
+//			s.cadastrar(u);
+//		};
+//	}
 
 }
