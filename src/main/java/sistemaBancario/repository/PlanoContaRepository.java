d9d8249e90d15b88af57b3a933fbc027371a3e93
Raphael Borges <rah-borges@hotmail.com>
2021-01-30 15:35:19 -0300

    refact(repositories):convt to spring data #69 #70 #71

--- src/main/java/sistemaBancario/repository/PlanoContaRepository.java
+++ src/main/java/sistemaBancario/repository/PlanoContaRepository.java
@@ -1,12 +1,12 @@
 package sistemaBancario.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import sistemaBancario.models.PlanoConta;
 
 import java.util.List;
 
-public interface PlanoContaRepository extends JpaRepository<PlanoConta, Long> {
+public interface PlanoContaRepository extends CrudRepository<PlanoConta, Long> {
 
 	PlanoConta getPlanoByFinalidadeAndUsuarioLogin(String finalidade, String login);
   
