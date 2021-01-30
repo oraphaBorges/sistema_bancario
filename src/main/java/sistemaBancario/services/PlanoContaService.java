e1ccfac6bc30737d6add87b2f848608d3c3257ff
Evictory <emerson_vitorio@live.com>
2021-01-30 11:02:05 -0300

    refactor(planocontaservice): Método atualizar refatorado - inserção #72

--- src/main/java/sistemaBancario/services/PlanoContaService.java
+++ src/main/java/sistemaBancario/services/PlanoContaService.java
@@ -2,20 +2,35 @@ package sistemaBancario.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
 import sistemaBancario.models.PlanoConta;
 import sistemaBancario.repository.PlanoContaRepository;
 
+import javax.persistence.NoResultException;
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
 public class PlanoContaService {
-	
 	@Autowired
 	private PlanoContaRepository repository;
-	
+
+	@Transactional
 	public void cadastrar(String finalidade) {
 		PlanoConta plano = repository.getPlanoByFinalidade(finalidade);
-		if (plano == null)
+		if (plano != null)
 			throw new IllegalStateException("Finalidade já cadastrada anteriormente");
 
 		repository.save(new PlanoConta(finalidade));
+	}
+
+	@Transactional
+	public void atualizar(long id, String finalidade){
+		PlanoConta planoBD = repository.findById(id).orElseThrow(() -> new NoResultException("Plano de conta não registrado"));
+		planoBD.setFinalidade(finalidade);
+
+		repository.save(planoBD);
 	}
 	
 }
