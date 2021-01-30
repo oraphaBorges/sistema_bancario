751d62549ff22853aa323140705e227cd17ea91a
Evictory <emerson_vitorio@live.com>
2021-01-30 20:05:13 -0300

    refactor(lancamentoDTO): adicionado parametros amigáveis para o front-end #88

--- src/main/java/sistemaBancario/dto/LancamentoDTO.java
+++ src/main/java/sistemaBancario/dto/LancamentoDTO.java
@@ -5,9 +5,12 @@ import java.time.LocalDate;
 public class LancamentoDTO {
 	
 	private LocalDate date; 
-	private Long contaOrigem; 
-	private Long contaDestino; 
+	private Long contaOrigem;
+	private String loginOrigem;
+	private Long contaDestino;
+	private String loginDestino;
 	private Long planoConta;
+	private String planoContaFinalidade;
 	private double valor;
 	private String descricao;
 			
@@ -72,14 +75,23 @@ public class LancamentoDTO {
 		this.descricao = descricao;
 	}
 
-
 	public Long getPlanoConta() {
 		return planoConta;
 	}
 
-
 	public void setPlanoConta(Long planoConta) {
 		this.planoConta = planoConta;
-	}	
-		
+	}
+
+	public void setLoginOrigem(String loginOrigem) {
+		this.loginOrigem = loginOrigem;
+	}
+
+	public void setLoginDestino(String loginDestino) {
+		this.loginDestino = loginDestino;
+	}
+
+	public void setPlanoContaFinalidade(String planoContaFinalidade) {
+		this.planoContaFinalidade = planoContaFinalidade;
+	}
 }
