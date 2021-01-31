c5b1eac9470882646cb3fc8f0e665a11ade3b572
Evictory <emerson_vitorio@live.com>
2021-01-31 19:32:22 -0300

    fix

--- src/main/java/sistemaBancario/security/WebSecurityConfigs.java
+++ src/main/java/sistemaBancario/security/WebSecurityConfigs.java
@@ -29,7 +29,7 @@ public class WebSecurityConfigs extends WebSecurityConfigurerAdapter{
 		.authorizeRequests()
 		.antMatchers(SWAGGER_WHITELIST).permitAll()
 		.antMatchers("/login").permitAll()
-		.antMatchers("/usuario/criar").permitAll()
+		.antMatchers("/usuarios/criar").permitAll()
 		.anyRequest().authenticated()
 		.and()
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
