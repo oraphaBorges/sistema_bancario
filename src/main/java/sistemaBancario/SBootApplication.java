7406895b3270a83eee952589c4670fd379e245e8
Evictory <emerson_vitorio@live.com>
2021-01-31 19:17:02 -0300

    test(main): comentando inicialização

--- src/main/java/sistemaBancario/SBootApplication.java
+++ src/main/java/sistemaBancario/SBootApplication.java
@@ -25,27 +25,27 @@ public class SBootApplication {
 	public CommandLineRunner run(UsuarioService us, ContaService cs
 			) throws Exception {
 		return args -> {
-		
-			UsuarioDTO u,u2;
-			u = new UsuarioDTO("22222222222", "DTO.black", "DTO.nome", "123456");
-			us.cadastrar(u);
-			u = new UsuarioDTO("33333333333","admin","admin","1234567");
-			us.cadastrar(u);
-			
-			u = us.buscar("DTO.black");
-			System.out.println(u.getLogin());
-			u2 = us.buscar("admin");
-			System.out.println(u2.getLogin());
-			
-		
-			ContaSimplesDTO origem = new ContaSimplesDTO(u.getLogin(),Sigla.POUPANCA);
-			ContaSimplesDTO destino = new ContaSimplesDTO(u2.getLogin(),Sigla.POUPANCA);
-			
-			System.out.println(origem + " " + destino);
-		
-			cs.depositar(new LancamentoDTO(origem,100,origem,"PAGAMENTO","Gardar DIMDIM"));
-			cs.pagar(new LancamentoDTO(origem,30,origem,"PAGAMENTO","Pagar Boleto"));
-			cs.transferir(new LancamentoDTO(origem,40,destino,"TRANSFERENCIA","Pagar Agiota"));						
+//
+//			UsuarioDTO u,u2;
+//			u = new UsuarioDTO("22222222222", "DTO.black", "DTO.nome", "123456");
+//			us.cadastrar(u);
+//			u = new UsuarioDTO("33333333333","admin","admin","1234567");
+//			us.cadastrar(u);
+//
+//			u = us.buscar("DTO.black");
+//			System.out.println(u.getLogin());
+//			u2 = us.buscar("admin");
+//			System.out.println(u2.getLogin());
+//
+//
+//			ContaSimplesDTO origem = new ContaSimplesDTO(u.getLogin(),Sigla.POUPANCA);
+//			ContaSimplesDTO destino = new ContaSimplesDTO(u2.getLogin(),Sigla.POUPANCA);
+//
+//			System.out.println(origem + " " + destino);
+//
+//			cs.depositar(new LancamentoDTO(origem,100,origem,"PAGAMENTO","Gardar DIMDIM"));
+//			cs.pagar(new LancamentoDTO(origem,30,origem,"PAGAMENTO","Pagar Boleto"));
+//			cs.transferir(new LancamentoDTO(origem,40,destino,"TRANSFERENCIA","Pagar Agiota"));
 			
 		};
 	}
