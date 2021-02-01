9ab67a4655fc6d39ac0657c5e3e2d197b619cf57
Raphael Borges <rah-borges@hotmail.com>
2021-01-31 21:16:55 -0300

    refact(no repeat signup):add try catch no command line

--- src/main/java/sistemaBancario/SBootApplication.java
+++ src/main/java/sistemaBancario/SBootApplication.java
@@ -1,4 +1,5 @@
 package sistemaBancario;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,36 +18,39 @@ public class SBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SBootApplication.class, args);
 	}
-	
+
 	static void setB(byte b) {
-		
+
 	}
+
 	@Bean
-	public CommandLineRunner run(UsuarioService us, ContaService cs
-			) throws Exception {
+	public CommandLineRunner run(UsuarioService us, ContaService cs) throws Exception {
 		return args -> {
-		
-			UsuarioDTO u,u2;
-			u = new UsuarioDTO("22222222222", "DTO.black", "DTO.nome", "123456");
-			us.cadastrar(u);
-			u = new UsuarioDTO("33333333333","admin","admin","1234567");
-			us.cadastrar(u);
-			
+
+			UsuarioDTO u, u2;
+			try {
+				u = new UsuarioDTO("22222222222", "DTO.black", "DTO.nome", "123456");
+				us.cadastrar(u);
+				u = new UsuarioDTO("33333333333", "admin", "admin", "1234567");
+				us.cadastrar(u);
+			} catch (Exception e) {
+
+			}
+
 			u = us.buscar("DTO.black");
 			System.out.println(u.getLogin());
 			u2 = us.buscar("admin");
 			System.out.println(u2.getLogin());
-			
-		
-			ContaSimplesDTO origem = new ContaSimplesDTO(u.getLogin(),Sigla.POUPANCA);
-			ContaSimplesDTO destino = new ContaSimplesDTO(u2.getLogin(),Sigla.POUPANCA);
-			
+
+			ContaSimplesDTO origem = new ContaSimplesDTO(u.getLogin(), Sigla.POUPANCA);
+			ContaSimplesDTO destino = new ContaSimplesDTO(u2.getLogin(), Sigla.POUPANCA);
+
 			System.out.println(origem.login + " " + destino.login);
-		
-			cs.depositar(new LancamentoDTO(origem,100,origem,"PAGAMENTO","Gardar DIMDIM"));
-			cs.pagar(new LancamentoDTO(origem,30,origem,"PAGAMENTO","Pagar Boleto"));
-			cs.transferir(new LancamentoDTO(origem,40,destino,"TRANSFERENCIA","Pagar Agiota"));						
-			
+
+			cs.depositar(new LancamentoDTO(origem, 100, origem, "PAGAMENTO", "Gardar DIMDIM"));
+			cs.pagar(new LancamentoDTO(origem, 30, origem, "PAGAMENTO", "Pagar Boleto"));
+			cs.transferir(new LancamentoDTO(origem, 40, destino, "TRANSFERENCIA", "Pagar Agiota"));
+
 		};
 	}
 
