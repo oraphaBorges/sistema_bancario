b18061af849fd13d79e848c7c69405a07539dbd2
Evictory <emerson_vitorio@live.com>
2021-02-10 09:29:29 -0300

    feat(userservice): add user service #148

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,21 @@
+import { UserService } from './service/UserService.js'
+
 $(document).ready(function(){
   $('.cpf').mask('000.000.000-00', {reverse: true});
 });
+
+let data = {
+  cpf: '32440909076',
+  login: 'emersommmmmm',
+  nome: 'emers',
+  senha: '123123'
+};
+
+// UserService.register(data);
+
+let data_login = {
+  usuario: 'emersombra',
+  senha: '123123'
+};
+
+// UserService.do_login(data_login);
