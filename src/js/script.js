527c652bafa095afd7b6cfa768bbbed46eb96403
Evictory <emerson_vitorio@live.com>
2021-02-10 10:08:14 -0300

    feat(index.html): add event listener to connect with userService register #156

--- src/js/script.js
+++ src/js/script.js
@@ -4,14 +4,44 @@ $(document).ready(function(){
   $('.cpf').mask('000.000.000-00', {reverse: true});
 });
 
-let data = {
-  cpf: '32440909076',
-  login: 'emersommmmmm',
-  nome: 'emers',
-  senha: '123123'
-};
+const register_form = document.getElementById('register-account');
 
-// UserService.register(data);
+if(register_form){
+  register_form.addEventListener('submit', e => {
+    e.preventDefault();
+  
+    const button = document.getElementById('register-new-account');
+    const buttonText = document.getElementById('button-text');
+    buttonText.innerHTML = 'Carregando...';
+    button.disabled = true;
+  
+    let data = { cpf: '', login: '', nome: '', senha: '' };
+  
+    for(let i = 0; i < register_form.elements.length; i++) {
+      const element = register_form.elements[i];
+  
+      if(!element.value && element.id !== 'register-new-account'){
+        alert(`Por gentileza, preencha o seguinte campo: ${element.getAttribute("description")}`);
+        buttonText.innerHTML = 'Continuar';
+        button.disabled = false;
+  
+        return;
+      }
+  
+      if(element.id !== 'confirmar_senha' && element.id !== 'register-new-account')
+        data[element.id] = element.value;
+  
+    }
+    
+    data.cpf = data.cpf.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,'');
+  
+    setTimeout(async () => {
+      await UserService.register(data);
+    }, 1000);
+  })
+
+}
+
 
 let data_login = {
   usuario: 'emersombra',
