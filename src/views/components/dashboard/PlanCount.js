3b8fdf878ecbfd4c2fdd2daf9ba8729779fe79ac
RafaelMarangoni <rafael.dmarangoni89@gmail.com>
2021-02-12 23:49:04 -0300

    feat(AcountPlan): cards plano de conta #174

--- src/views/components/dashboard/PlanCount.js
+++ src/views/components/dashboard/PlanCount.js
@@ -1,11 +1,15 @@
+import { dashboardHeader as Header, card as Card} from '../index';
 
 const PlanCount = {
   
     render:  () => {
         let view = `
-
-           ${html}
-           
+        <div class="content-default content-flex content-justify">
+            <div class="section">
+            ${ Header.render('Olá, <strong>Pessoa lendo</strong>, seja bem-vin!') }
+            </div>
+         </div>
+         ${newAcountPlan}           
         `;
 
         return view
@@ -18,26 +22,37 @@ const PlanCount = {
 
 }
 
-let html = 
-`  <div>
-<div>
-  <input type="text" for="tipoDeConta">
-  <select name="" id="tipoDeConta">
-    <option value="Debito">Debito</option>
-    <option value="Credito">Credito</option>
-    <option value="Poupanca">Poupanca</option>
-    <input type="submit" value="Cadastrar">
-  </select>
-</div>
+let newAcountPlan = 
+`
+<div class="content content-default content-flex content-justify">
+    <div class="card ">
+        <h2>Cadastre novo plano</h2>
+        <form id="login_form" class="card-form mt-5">
+            <input  type="text" placeholder="Digite novo plano">            
+            <button id="button_submit " type="submit" class="btn btn-main-outline btn-rounded">Cadastrar</button>
+        </form>            
+    </div> 
+    <div class="card ">
+        <h2>Planos Cadastrados</h2>
+        <form id="login_form" class="card-form ">
+            <ul class="text-card--default text-center card-list--none padding--dault">
+                <li>Academia</li>
+                <li>Imposto</li>
+                <li>Compra</li>
+                <li>IPVA</li>
+                <li>IPTU</li>
+                <li>Declaração Imposto</li>
+                <li>Escola</li>
+                <li>Viagens</li>
+            </ul>
+        </form>            
+    </div>  
+    
 </div>
 
-<div>
-    <div>
-        <h1>PLanos já cadastrados</h1>
-    </div>
-</div>
+
+
 `
-  
-   
+
 
 export default PlanCount;
