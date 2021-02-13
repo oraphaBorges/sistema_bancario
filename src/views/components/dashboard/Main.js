8b59c490e41788c5f58b48f415a54dc7db51589e
Evictory <emerson_vitorio@live.com>
2021-02-13 15:59:38 -0300

    refactor(dashboard): refactor de card do dashboard para adaptaçao ao modelo da api #207

--- src/views/components/dashboard/Main.js
+++ src/views/components/dashboard/Main.js
@@ -12,15 +12,10 @@ const Main = {
                 ${ Header.render('Olá, <strong>Pessoa lendo</strong>, seja bem-vin!') }
                 </div>
             </div>
-            <div class="content-default content-flex content-justify">
-                <div class="section content-flex">
-                ${section1}
-                </div>
-            </div>
-            <div class="content-default content-flex content-justify">
-                <div class="section">
-                ${section2}
-                </div>
+            <div class="accounts content-default content-flex content-justify">
+                ${ accountCard }
+                ${ accountCard }
+                ${ accountCard }
             </div>
         </div>
         `;
@@ -29,63 +24,44 @@ const Main = {
     },
 
     after_render: () => { 
-        console.log('teste')
+        
     },
 }
 
 export default Main;
 
-let section1 = `
-    <div class="card-dashboard--default m-1 flex-wrap--40">
-        <div class="content-intern--default content-flex content-align--center">
-            <img src="${ dollarIcon }"  class="content-icon--default" alt="">
-            <span class="text-card--default">Conta</span>
-        </div>
-        <div class="content-intern--default">
-            <span class="text-card--default">Saldo disponível</span>
-            <h2 class="text-money">R$: 2.354,00</h2>
-        </div>
-        <div class="content-intern--default content-flex content-align--center">
-            <span class="text-card--default content-flex--1">Transações</span>
-            <h2 class="text-money text-color--green">R$: 2.354,00</h2>
-        </div>
-    </div>
-    <div class="card-dashboard--default m-1 flex-wrap--40">
+let accountCard = `
+    <div class="card-dashboard--default m-1">
         <div class="content-intern--default content-flex content-align--center">
             <img src="${ cardIcon }"  class="content-icon--default" alt="">
             <span class="text-card--default">Conta crédito</span>
         </div>
-        <div class="content-intern--default">
-            <span class="text-card--default">Fatura atual</span>
-            <h2 class="text-money text-color--blue">R$: 522,12</h2>
-        </div>
-        <div class="content-intern--default content-flex content-align--center">
-            <span class="text-card--default content-flex--1">Limite disponível</span>
-            <h2 class="text-money text-color--green">R$: 9.120,88</h2>
-        </div>
-    </div>
-`
-let latestTransactions = `
-    <div class="content-intern--default content-flex" id="transaction">
-        <img src="${ cardOpenedIcon }" class="content-icon--default" alt="">
-        <div class="content-transaction content-flex--1 content-flex--column">
-            <div class="payment-description content-flex content-flex--1 content-justify--between content-align--center">
-                <span class="text-color--black"><strong>Compra no débito</strong></span>
-                <span class="text-color--grey text-size--12">24 de jan.</span>
+        <div class="content-intern--default content-flex content-justify--between">
+            <div class="content-intern--default">
+                <span class="text-card--default">Fatura atual</span>
+                <h2 class="text-money text-color--blue">R$: 522,12</h2>
             </div>
-            <span class="text-card--default content-padding--default">Gama Academy</span>
-            <span class="text-color--black text-weight--bold text-size--20">R$ 178,10</span>
+            <div class="content-intern--default">
+                <span class="text-card--default">Limite disponível</span>
+                <h2 class="text-money text-color--green">R$: 9.120,88</h2>
+            </div>
+        </div>
+        <div id="latest_transactions">
+            <div class="content-intern--default content-flex content-justify--between">
+                <span class="text-card--default">Últimos lançamentos</span>
+                <a id="see_more" class="text-card--ancor">Ver extrato completo</a>
+            </div>
+            <div class="content-intern--default content-flex" id="transaction">
+                <img src="${ cardOpenedIcon }" class="content-icon--default" alt="">
+                <div class="content-transaction content-flex--1 content-flex--column">
+                    <div class="payment-description content-flex content-flex--1 content-justify--between content-align--center">
+                        <span class="text-color--black"><strong>Compra no débito</strong></span>
+                        <span class="text-color--grey text-size--12">24 de jan.</span>
+                    </div>
+                    <span class="text-card--default content-padding--default">Gama Academy</span>
+                    <span class="text-color--black text-weight--bold text-size--20">R$ 178,10</span>
+                </div>
+            </div>
         </div>
     </div>
 `
-let section2 = `
-    <div class="card-dashboard--default m-1">
-        <div class="content-intern--default content-flex content-align--center">
-            <img src="${ dollarIcon }" class="content-icon--default" alt="">
-            <span class="text-card--default">Últimos lançamentos</span>
-            <a id="see_more" class="text-card--ancor">Ver extrato completo</a>
-        </div>
-        ${ latestTransactions }
-    </div>
-`
-
