f11f97c09a8895967c72e93fb26a38a7103e3df4
Raphael Borges <rah-borges@hotmail.com>
2021-02-14 12:13:48 -0300

    feat(home):bloco celulares

--- src/views/pages/home/Home.js
+++ src/views/pages/home/Home.js
@@ -4,6 +4,9 @@ import { nav as Nav } from '../../components/index';
 
 import UserService from '../../../service/UserService';
 
+import celularWhite from '../../../img/cell-white-bg.png';
+import celularBlack from '../../../img/cell-black-bg.png';
+
 let Home = {
   is_private: false,
 
@@ -11,10 +14,12 @@ let Home = {
       let view = `
         ${await Nav}
           <div id="bloco-cadastro-login" class="container bloco">
-          ${blocoLogin}
-          ${await Form}
+            ${blocoLogin}
+            ${await Form}
           </div>
-          ${html}
+          <div id="bloco-celulares" class="bloco">
+            ${blocoCelulares}
+          </div>
         ${await Footer}
       `;
 
@@ -32,13 +37,7 @@ let Home = {
       for(let i = 0; i < register_form.elements.length; i++) {
         const element = register_form.elements[i];
 
-        if(!element.value && element.id !== 'button_submit'){
-          alert(`Por gentileza, preencha o seguinte campo: ${element.getAttribute("description")}`);
-
-          return;
-        }
-
-        if(element.id === 'CPF'){
+        if(element.id === 'cpf'){
           //criar função util de validar CPF;
         }
 
@@ -46,7 +45,6 @@ let Home = {
           data[element.id] = element.value;
 
       }
-
       await UserService.register(data);
     });
   }
@@ -54,37 +52,46 @@ let Home = {
 
 let blocoLogin = `
   <div class="bloco-item bloco-content-login">
-      <h2>Gama Bank</h2>
-      <p>é um projeto de nossos alunos.</p>
-      <p>Já tem conta?</p>
-      <a href="#/login"><button class="btn btn-md btn-rounded btn-gama">Acessar</button></a>
+    <h2>Gama Bank</h2>
+    <p>é um projeto de nossos alunos.</p>
+    <p>Já tem conta?</p>
+    <a href="#/login"><button class="btn btn-md btn-rounded btn-gama">Acessar</button></a>
   </div>
 `
 
-let html =
-`    <div class="container-width--full container-height--full bg-grey">
-      <div class="content-default content-flex">
-        <div class="flex-wrap container-height--full container-width--full">img 1</div>
-        <div class="flex-wrap container-height--full container-width--full">img 2</div>
-      </div>
-    </div>
-    <div class="container-width--full container-height--full bg-purple">
-      <div class="content-flex container-height--full container-width--full">
-        <div class="flex-wrap">Div1</div>
-        <div class="flex-wrap">Dvi2</div>
-      </div>
-    </div>
-    <div class="container-width--full container-height--full bg-green">
-      <div class="content-default content-flex">
-        <p>text 1</p>
-      </div>
-    </div>
-    <div class="container-width--full container-height--full bg-grey">
-    <div class="content-flex container-height--full container-width--full">
-      <div class="flex-wrap">Div1</div>
-      <div class="flex-wrap">Dvi2</div>
-    </div>
-
+let blocoCelulares=`
+  <div class="bloco-item bloco-content-celular  bg-dark">
+    <h1 class="text-gama text-center">CONTA DIGITAL</h1>
+    <img src="${celularWhite}" alt="celular branco">
+    <p class="text-gama">
+      GamaAcademy Bank Line S.A. - CNPJ 00.000.000/0000-00 <br>
+      Rua Fictícia, 999 - 00000-000 - São Paulo, SP <br>
+      Este é um projeto de avaliação | GamaAcademy | Accenture | 2021
+    </p>
+  </div>
+  <div class="bloco-item bloco-content-celular  bg-light">
+    <h1 class="text-purple text-center">CARTÃO DE CRÉDITO</h1>
+    <img src="${celularBlack}" alt="celular preto">
+    <p>
+      GamaAcademy Bank Line S.A. - CNPJ 00.000.000/0000-00 <br>
+      Rua Fictícia, 999 - 00000-000 - São Paulo, SP <br>
+      Este é um projeto de avaliação | GamaAcademy | Accenture | 2021
+    </p>
+  </div>
 `
+
+let blocoAnuidade=`
+  <div class="bloco-item bloco-content-anuidade">
+    <h2 class="text-light">Zero anuidade</h2>
+    <p class="text-light">
+      Burocracia custa caro. Somos eficientes<br>
+      para você não ter que pagar tarifas.
+    </p>
+  </div>
+  <div class="bloco-item bloco-content-anuidade text-center">
+    <h1 class="text-gama">0,0</h2>
+  </div>
+`
+
 
 export default Home;
