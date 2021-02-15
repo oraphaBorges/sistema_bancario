0d3f75c1144e90d29156209b2ed6e5180cfb9e86
Evictory <emerson_vitorio@live.com>
2021-02-15 18:35:35 -0300

    feat(userService): add userService file #257

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+  }
+
+  doLogin(){
   }
 
 }
---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export class Config {
+  public readonly baseURL = 'https://gama-bankline-desbugados.herokuapp.com/';
+}
