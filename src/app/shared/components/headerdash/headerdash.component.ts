83f535a83f80adb547de7bfd56fe108294c3f0d1
Evictory <emerson_vitorio@live.com>
2021-02-17 13:25:36 -0300

    feat(changePass): add temporary implementation 312

--- src/app/shared/components/headerdash/headerdash.component.ts
+++ src/app/shared/components/headerdash/headerdash.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-headerdash',
@@ -10,12 +12,23 @@ export class HeaderDashComponent implements OnInit {
     @Input()
     message = "Implemente pls"
 
-    updateIcon = '../../../../assets/img/svg/update.svg'
-    logoutIcon = '../../../../assets/img/svg/logout.svg'
+    now = new Date();
 
-    constructor() { }
+    updateIcon = '../../../../assets/img/svg/update.svg';
+    logoutIcon = '../../../../assets/img/svg/logout.svg';
+
+    constructor(private router: Router, private authService: AuthService) { }
 
     ngOnInit(): void {
     }
 
+    doLogout(){
+      this.authService.doLogout();
+      this.router.navigate(['login']);
+    }
+
+    reload(){
+      console.log('teste')
+      location.reload();
+    }
 }
